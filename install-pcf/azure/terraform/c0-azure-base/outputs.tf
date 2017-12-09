614fa927a40e8a3af433443dbb89cc05c2712e4e
Yadhav Jayaraman <yjayaraman@pivotal.io>
2017-12-09 01:22:26 -0500

    fixed typo

--- install-pcf/azure/terraform/c0-azure-base/outputs.tf
+++ install-pcf/azure/terraform/c0-azure-base/outputs.tf
@@ -3,6 +3,6 @@
 ////////                          /////////////
 ///////////////////////////////////////////////
 
-output "mysql_dns" {
-  value = "mysql.${azurerm_dns_a_record.mysql.zone_name}"
+//////output "mysql_dns" {
+//////  value = "mysql.${azurerm_dns_a_record.mysql.zone_name}"
 }
