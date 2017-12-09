926abf09be6c0a56df05e4fcbe30b389c1c033d7
Yadhav Jayaraman <yjayaraman@pivotal.io>
2017-12-09 02:35:35 -0500

    fixed config director errors

--- install-pcf/azure/tasks/iaas/config-director.sh
+++ install-pcf/azure/tasks/iaas/config-director.sh
@@ -160,7 +160,7 @@ security_configuration=$(
     }'
 )
 
-echo '{"director_configuration": '+"${director_configuration}"+'}'
+echo '{"networks-configuration": '"${networks-configuration}"'}'
 
 om-linux \
   --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
